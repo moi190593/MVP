4b6f66a74f821d48a8ff5620234e5a591aa916a0
MOISES GARCIA PEDEMONTE <moises.garcia-pedemonte.ext@inetum.com>
2023-04-04 09:52:25 +0200

    Primer commit

---
+++ proyecto/index.js
@@ -0,0 +1,18 @@
+const MIDA = 10;
+
+console.log(String.fromCharCode(65,66,67,105))
+function Nombre (){
+    let password = null;
+    
+    for (i=0; i < MIDA; i++){
+        if(i < MIDA/2){
+            numRandom = Math.floor(Math.random() * 9);
+
+            password = password + numRandom
+        }else{          
+            letrarandom
+            password
+        }
+    }
+
+}
